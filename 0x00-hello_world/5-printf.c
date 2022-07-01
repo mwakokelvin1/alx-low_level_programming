0e9791a052260ad9b4983456371db5ed91af177e
mwakokelvin1 <mwakokelvin1@gmail.com>
2022-07-01 12:48:11 -0700

    kevo

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *Description: main - prints out a string.
+ *Return :Always (Success);
+ */
+int main(void) 
+{  
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
